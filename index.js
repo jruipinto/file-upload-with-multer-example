61d5026237f47f2cf511fb49fb23f706aa99a489
jruipinto <44852805+jruipinto@users.noreply.github.com>
2020-08-27 21:25:07 +0100

    feat: add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const multer = require("multer");
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
 const app = express();
-var path = require('path');
 const port = 3030;
 
-var storage = multer.diskStorage({
-    destination: "./uploads/",
+const storage = multer.diskStorage({
+    destination: './uploads/',
     filename: function (req, file, cb) {
         cb(null, file.originalname);
     },
@@ -13,12 +14,14 @@ var storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.post("/upload", upload.any(), function (req, res, next) {
+app.use(cors());
+
+app.post('/upload', upload.any(), function (req, res, next) {
     console.log(req.files || req.file);
     if (req.files || req.file) {
         return res.status(200).json(req.files || req.file);
     }
-    return res.status(400).json({ msg: "PLEASE UPLOAD FILE" });
+    return res.status(400).json({ msg: 'PLEASE UPLOAD FILE' });
 });
 
 app.get('/', (req, res, next) => {
